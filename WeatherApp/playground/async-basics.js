f962a20b7a4c62d0b6e4f6311f122a1063715be0
Vimlesh <kumarvimlesh007@gmail.com>
2018-05-21 19:09:06 +0530

    weather add started

---
+++ WeatherApp/playground/async-basics.js
@@ -0,0 +1,18 @@
+console.log('Straing app');
+setTimeout(() => {
+console.log('inside of callback');
+},2000);
+setTimeout(() => {
+  console.log('second tome out');
+},0);
+console.log('Finishing up');
+console.log('Finishing up');
+console.log('Finishing up');
+console.log('Finishing up');
+console.log('Finishing up');
+console.log('Finishing up');
+console.log('Finishing up');console.log('Finishing up');
+console.log('Finishing up');
+console.log('Finishing up');
+console.log('Finishing up');
+console.log('Finishing up');
