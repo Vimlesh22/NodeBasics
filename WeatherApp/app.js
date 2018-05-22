d5b1f06f539d995dfa31d762a165c49ce1046c87
Vimlesh <kumarvimlesh007@gmail.com>
2018-05-22 15:55:17 +0530

    weather app cotinued on 22may2018

--- WeatherApp/app.js
+++ WeatherApp/app.js
@@ -1,8 +1,22 @@
-const request = require('request');
-
-request({
-  url :'https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia',
-  json :true
-},(error,response,body) => {
-console.log(`Address :${body.results[0].formatted_address}`);
+const yargs = require('yargs');
+const geocode = require('./geocode/geocode.js')
+const argv = yargs
+.options({
+  a:{
+    demand:true,
+    alias:'address',
+    description:'Address to fetch weather for',
+    string:true
+  }
 })
+.help()
+.alias('help','h')
+.argv;
+var myCallback = (errorMessage,results) => {
+  if (errorMessage) {
+    console.log(errorMessage);
+  }else {
+    console.log(JSON.stringify(results,undefined,2));
+  }
+}
+geocode.geocodeAddress(argv.address,myCallback);
